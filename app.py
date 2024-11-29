da421e10270f1b19c5817054f42a6a4bba1a647d
saurabh sonakia <96692293+Saurabh200021@users.noreply.github.com>
2024-11-29 16:17:55 +0530

    ok

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ app.secret_key = 'supersecretkey'  # Required for flashing messages
 # Excel file path
 excel_file_path = 'Vehicle_data01.xlsx'
 
+
+
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -31,6 +33,28 @@ def State_History():
 @app.route('/dtc_description')
 def dtc_description():
     return render_template('dtc_description.html')
+
+@app.route('/Segment2')
+def Segment2():
+    return render_template('Segment2.html')
+
+@app.route('/Segment3')
+def Segment3():
+    return render_template('Segment3.html')
+
+@app.route('/Segment4')
+def Segment4():
+    return render_template('Segment4.html')
+
+@app.route('/Segment5')
+def Segment5():
+    return render_template('Segment5.html')
+
+@app.route('/Profile')
+def Profile():
+    return render_template('Profile.html')
+
+
 
 
 @app.route('/get-data', methods=['POST'])
